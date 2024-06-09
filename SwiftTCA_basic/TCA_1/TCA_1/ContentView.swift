1d2cd00453593243b01ec871b673f4646596a7f9
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-06-09 23:48:55 +0900

    tca_basic add

--- SwiftTCA_basic/TCA_1/TCA_1/ContentView.swift
+++ SwiftTCA_basic/TCA_1/TCA_1/ContentView.swift
@@ -6,19 +6,97 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+// 랜덤 데이터 추가하기
+
+enum Animal: String, Equatable {
+    case dog = "Dog"
+    case cat = "Cat"
+}
+
+@Reducer
+struct RanAni {
+    @ObservableState
+    struct State: Equatable {
+        var dogcnt: Int = 0
+        var catcnt: Int = 0
+    }
+    
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+        case addcntButtonTapped(Animal)
+    }
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .addcntButtonTapped(let animal):
+                switch animal {
+                case .dog:
+                    state.dogcnt += 1
+                case .cat:
+                    state.catcnt += 1
+                }
+                return .none
+            case .binding:
+                return .none
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView: View {
+    
+    @Bindable var store: StoreOf<RanAni>
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                VStack {
+                    Text("DOG")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("\(store.dogcnt)")
+                        .font(.title)
+                }
+                .padding()
+                
+                VStack {
+                    Text("CAT")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("\(store.catcnt)")
+                        .font(.title)
+                }
+                .padding()
+
+            }
+            .padding()
+            
+            Text("which is better?")
+            
+            HStack {
+                Button(action: {
+                    store.send(.addcntButtonTapped(.dog))
+                }, label: {
+                    Text("Dog")
+                })
+                .padding()
+                
+                Button(action: {
+                    store.send(.addcntButtonTapped(.cat))
+                }, label: {
+                    Text("Cat")
+                })
+                .padding()
+
+            }
+        }
+        .padding()
+    }
 }
--- SwiftTCA_basic/TCA_1/TCA_1/TCA_1App.swift
+++ SwiftTCA_basic/TCA_1/TCA_1/TCA_1App.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCA_1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(initialState: RanAni.State()) {
+                    RanAni()
+                }
+            )
         }
     }
 }
