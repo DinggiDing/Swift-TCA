9851e9560ee50b3aea5a870d1f4b699829323b28
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-06-09 21:41:41 +0900

    TCA_basic

---
+++ SwiftTCA_basic/TCA_1/TCA_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_1
+//
+//  Created by 성재 on 6/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTCA_basic/TCA_1/TCA_1/TCA_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_1App.swift
+//  TCA_1
+//
+//  Created by 성재 on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
