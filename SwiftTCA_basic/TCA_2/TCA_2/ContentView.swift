e4af6df77e4b9393370d5f2eae3dfe44c3791cbf
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-06-17 14:29:55 +0900

    24/06/17 : TCA setting

--- SwiftTCA_basic/TCA_2/TCA_2/ContentView.swift
+++ SwiftTCA_basic/TCA_2/TCA_2/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
     var body: some View {
--- SwiftTCA_basic/TCA_2/TCA_2/TCA_2App.swift
+++ SwiftTCA_basic/TCA_2/TCA_2/TCA_2App.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCA_2App: App {
